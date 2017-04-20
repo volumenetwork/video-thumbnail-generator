4c3c9f0c89756ff48f2637b3a286b964e32059eb
James Irving-Swift <james@irving-swift.com>
2017-04-20 12:51:22 +0200

    change code coverage threshold

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -89,7 +89,7 @@ gulp.task('coverage:report', () => (
         dir: './coverage',
       },
     }))
-    .pipe(istanbul.enforceThresholds({ thresholds: { global: 10 } }))
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 0 } }))
     .on('error', util.log)
 ));
 
