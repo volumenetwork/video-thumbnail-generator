7ca200ff5f985a2328eb46cfcb1c8661175b0f7f
James Irving-Swift <james@irving-swift.com>
2017-04-20 15:29:49 +0200

    added method to take one screenshot & updated comments

--- README.md
+++ README.md
@@ -6,17 +6,27 @@
 
 ```js
 import ThumbnailGenerator from 'volume-thumbnail-generator';
+// const ThumbnailGenerator = require('./build/index').default;
 
-var tg = new ThumbnailGenerator({
-  sourcePath: '/path/to/my/file.mp4',
-  thumbnailPath: '/var/tmp/screenshots/',
+const tg = new ThumbnailGenerator({
+  sourcePath: '/tmp/test.mp4',
+  thumbnailPath: '/tmp/',
 });
 
 tg.generate()
-  .then(function (filenames) {
-    console.log(filenames);
-  })
-  .catch( err => {
-    console.log(err);
-  });
+  .then(console.log);
+// [ 'test-thumbnail-320x240-0001.png' ]
+
+tg.generateOneByPercent(90)
+  .then(console.log);
+// [ 'test-thumbnail-320x240-0001.png',
+//  'test-thumbnail-320x240-0002.png',
+//  'test-thumbnail-320x240-0003.png',
+//  'test-thumbnail-320x240-0004.png',
+//  'test-thumbnail-320x240-0005.png',
+//  'test-thumbnail-320x240-0006.png',
+//  'test-thumbnail-320x240-0007.png',
+//  'test-thumbnail-320x240-0008.png',
+//  'test-thumbnail-320x240-0009.png',
+//  'test-thumbnail-320x240-0010.png' ]  
 ```
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import FfmpegCommand from 'fluent-ffmpeg';
 import Promise from 'bluebird';
+import _ from 'lodash';
 
+/**
+ * @class ThumbnailGenerator
+ */
 export default class ThumbnailGenerator {
+  /**
+   * @constructor
+   *
+   * @param {String} [opts.sourcePath] - 'full path to video file'
+   * @param {String} [opts.thumbanilPath] - 'path to where thumbnail(s) should be saved'
+   * @param {Number} [opts.percent]
+   * @param {String} [opts.size]
+   * @param {Logger} [opts.logger]
+   */
   constructor(opts) {
     this.sourcePath = opts.sourcePath;
     this.thumbailPath = opts.thumbnailPath;
-    this.percent = opts.percent || '90%';
+    this.percent = `${opts.percent}%` || '90%';
     this.logger = opts.logger || null;
+    this.size = opts.size || '320x240';
+    this.fileNameFormat = '%b-thumbnail-%r-%000i';
   }
 
+  /**
+   * @method getFfmpegInstance
+   *
+   * @return {FfmpegCommand}
+   *
+   * @private
+   */
   getFfmpegInstance() {
     return new FfmpegCommand({
       source: this.sourcePath,
@@ -16,9 +38,56 @@ export default class ThumbnailGenerator {
     });
   }
 
-  generate() {
+  /**
+   * Method to generate one thumbnail by being given a percentage value.
+   *
+   * @method generateOneByPercent
+   *
+   * @param {Number} percent
+   *
+   * @return {Promise}
+   *
+   * @public
+   *
+   * @async
+   */
+  generateOneByPercent(percent) {
+    if (percent < 0 || percent > 100) {
+      throw new Error('Perect must be a value from 0-100');
+    }
+
+    return this.generate({
+      count: 1,
+      timestamps: [`${percent}%`],
+    }).then(result => result.pop());
+  }
+
+  /**
+   * Method to generate thumbnails
+   *
+   * @method generate
+   *
+   * @param {String} [opts.folder]
+   * @param {Number} [opts.count]
+   * @param {String} [opts.size] - 'i.e. 320x320'
+   * @param {String} [opts.filename]
+   *
+   * @return {Promise}
+   *
+   * @public
+   *
+   * @async
+   */
+  generate(opts) {
+    const defaultSettings = {
+      folder: this.thumbailPath,
+      count: 10,
+      size: this.size,
+      filename: this.fileNameFormat,
+    };
+
     const ffmpeg = this.getFfmpegInstance();
-    const tnPath = this.thumbailPath;
+    const settings = _.assignIn(defaultSettings, opts);
     let filenameArray = [];
 
     return new Promise((resolve, reject) => {
@@ -34,10 +103,7 @@ export default class ThumbnailGenerator {
         .on('filenames', filenames)
         .on('end', complete)
         .on('error', reject)
-        .screenshots({
-          folder: tnPath,
-          count: 10,
-        });
+        .screenshots(settings);
     });
   }
 }
